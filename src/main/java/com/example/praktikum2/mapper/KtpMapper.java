f923464bad4ad49bce870ebacd9def78ef353c6a
Arsyan <syandi.2901@gmail.com>
2026-03-12 01:51:32 -0700

    Commit 5: Create mapper.KtpMapper

---
+++ src/main/java/com/example/praktikum2/mapper/KtpMapper.java
@@ -0,0 +1,18 @@
+package com.example.praktikum2.mapper;
+
+import com.example.praktikum2.dto.KtpDto;
+import com.example.praktikum2.entity.Ktp;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface KtpMapper {
+    KtpMapper INSTANCE = Mappers.getMapper(KtpMapper.class);
+
+    KtpDto toDto(Ktp ktp);
+
+    Ktp toEntity(KtpDto ktpDto);
+
+    void updateEntityFromDto(KtpDto ktpDto, @MappingTarget Ktp ktp);
+}
